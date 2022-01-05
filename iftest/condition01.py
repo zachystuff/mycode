aeabbef33045d7820731a6604616bd708d5c2dc1
zachystuff <zachyusuf@live.com>
2022-01-05 14:44:28 -0600

    lab 23

---
+++ iftest/condition01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    # create the string hostname
+    hostname = "MTG"
+    # test logic with 'if' statement
+    if hostname == "MTG":
+        print("The hostname was found to be MTG")
+
+main()
---
+++ iftest/condition02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def main():
+    hostname = input("What value should we set for hostname?")
+    if hostname == "MTG":
+        print("The hostname was found to be MTG")
+
+
+main()
+
