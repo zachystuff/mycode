fa66878059119fc4ea0f027c8989108a18a029a9
zachystuff <zachyusuf@live.com>
2022-01-06 13:05:41 -0600

    for loop intro python

---
+++ misc/forloopbasics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def main():
+    for i in range(4):
+        print(i)
+
+    for x in range(5):
+        print(x, end=" ")
+
+    fruitbowl : list[str] = ["apple","pear","grapes"]
+    for fruit in fruitbowl:
+        print("\n", fruit)
+
+    foo = open("ourfile.txt", "w")
+    for fruit in fruitbowl:
+        print(fruit, file=foo)
+    foo.close()
+
+
+if __name__ == "__main__":
+    main()
